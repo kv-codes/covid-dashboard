5e792330ab2f15f0d703ccf52f4012e7ae97e35f
eric-owen <65836532+eric-owen@users.noreply.github.com>
2020-10-20 21:45:28 -0400

    added state dropdown with javascript

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ const renderDropdown = () => {
     fetch('https://www.trackcorona.live/api/provinces')
         .then(response => response.json())
         .then(jsonData => {
-            let stateArray = []
             jsonData.data.forEach(function (state) {
                 if (state.country_code === 'us') {
-                    stateArray.push(state.location)
                     let statesDropdown = document.querySelector('#states')
                     let newOption = document.createElement('option')
                     newOption.setAttribute('value', state.location)
