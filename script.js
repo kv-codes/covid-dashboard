66ca6eae7bfedfd558a7ebbdf6e245cd4c928aa3
eric-owen <65836532+eric-owen@users.noreply.github.com>
2020-10-22 20:18:07 -0400

    added heat map

--- script.js
+++ script.js
@@ -16,3 +16,86 @@ const renderDropdown = () => {
 }
 
 renderDropdown()
+
+document.querySelector('#states').addEventListener('change', (event) => {
+
+    //console.log(event.target.value)
+    fetch('https://www.trackcorona.live/api/provinces')
+        .then(response => response.json())
+        .then(jsonData => {
+            jsonData.data.forEach(function (state) {
+                if (state.location === event.target.value) {
+                    console.log(state)
+                    let statsDiv = document.querySelector('#stats')
+                    statsDiv.innerHTML = `
+                    Confirmed cases: ${state.confirmed}
+                    Deceased: ${state.dead}
+                    Recovered: ${state.recovered}
+                    Last Updated: ${state.updated}
+                    `
+                }
+            })
+        })
+})
+
+
+
+// This example requires the Visualization library. Include the libraries=visualization
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=visualization">
+let map, heatmap;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 13,
+        center: {
+            lat: 37.775,
+            lng: -122.434
+        },
+        mapTypeId: "satellite",
+    });
+    heatmap = new google.maps.visualization.HeatmapLayer({
+        data: getPoints(),
+        map: map,
+    });
+}
+
+function toggleHeatmap() {
+    heatmap.setMap(heatmap.getMap() ? null : map);
+}
+
+function changeGradient() {
+    const gradient = [
+        "rgba(0, 255, 255, 0)",
+        "rgba(0, 255, 255, 1)",
+        "rgba(0, 191, 255, 1)",
+        "rgba(0, 127, 255, 1)",
+        "rgba(0, 63, 255, 1)",
+        "rgba(0, 0, 255, 1)",
+        "rgba(0, 0, 223, 1)",
+        "rgba(0, 0, 191, 1)",
+        "rgba(0, 0, 159, 1)",
+        "rgba(0, 0, 127, 1)",
+        "rgba(63, 0, 91, 1)",
+        "rgba(127, 0, 63, 1)",
+        "rgba(191, 0, 31, 1)",
+        "rgba(255, 0, 0, 1)",
+    ];
+    heatmap.set("gradient", heatmap.get("gradient") ? null : gradient);
+}
+
+function changeRadius() {
+    heatmap.set("radius", heatmap.get("radius") ? null : 20);
+}
+
+function changeOpacity() {
+    heatmap.set("opacity", heatmap.get("opacity") ? null : 0.2);
+}
+
+// Heatmap data: 500 Points
+function getPoints() {
+    return [
+        //new google.maps.LatLng(37.782551, -122.445368),
+
+    ];
+}
